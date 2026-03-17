062d094815aa5a5e01eb89fa5ab701c2720147f4
SeongminYoonha <unisgene@gmail.com>
2026-03-18 08:58:05 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -25,30 +25,22 @@ export const Location = () => {
           <div className="content">
             * 지하철 이용시
             <br />
-            지하철 2호선 <b>낙성대역 4번출구</b> 나와서
+            지하철 2호선, 4호선, 5호선 <b>동대문역사문화공원역 4번출구</b>
             <br />
-            → 첫번째 골목 끼고 좌회전
-            <br />→ 마을버스 <b>관악 02번</b> 승차
-            <br />→ <b>서울대후문·연구공원 정류장</b> 하차
+            지하철 3호선 <b>동대입구역 4번출구</b>
             <br />
-            → 길 건너 간판 참고해 도보로 100m 이동
-            <br />
-            검은색 피라미드 유리 건물입니다.
           </div>
           <div />
           <div className="content">
             * 버스 이용 시
             <br />
-            - 간선(파랑): 461, 641
+            - 간선(파랑): 144, 301, 420
             <br />
-            - 지선(초록): 5413, 5524, 5528
+            - 지선(초록): 7212
             <br />
-            반드시 <b>낙성대입구</b> 하차
-            <br />→ 마을버스 <b>관악 02번</b> 이용
-            <br />
-            이하 위와 동일합니다.
+            <b>장충동(동국대입구) 정류장</b>
           </div>
-        </div>
+          </div>
         <div className="location-info">
           <div className="transportation-icon-wrapper">
             <CarIcon className="transportation-icon" />
@@ -58,19 +50,11 @@ export const Location = () => {
           <div className="content">
             네이버 지도, 카카오 네비, 티맵 등 이용
             <br />
-            <b>서울대학교 연구공원 웨딩홀</b> 검색
+            <b>장충민영주차장</b> 검색
             <br />
-            - 주차 요금은 무료입니다.
+            ※ 주차 공간이 협소하오니 가급적 대중교통을 이용해 주시기 바랍니다.
             <br />
-            (주차장 이용 시 웨딩홀과 바로 연결)
-          </div>
-          <div />
-          <div className="content">
-            <b>
-              ※ 서울대학교 정, 후문을 통과할 경우 통행료가 발생하므로
-              유의바랍니다. 낙성대 방향으로 이용해주세요.
-            </b>
-          </div>
+
         </div>
       </LazyDiv>
     </>
