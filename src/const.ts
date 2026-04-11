4a580208f592cb67d93f568f7639dedec9ac348a
SeongminYoonha <unisgene@gmail.com>
2026-04-11 14:07:48 +0900

    Shorten account numbers in const.ts

--- src/const.ts
+++ src/const.ts
@@ -47,19 +47,19 @@ export const BRIDE_INFO = [
     relation: "신부",
     name: BRIDE_FULLNAME,
     phone: "010-0000-0000",
-    account: "KB은행 00027140293110093000",
+    account: "KB 27140293110093",
   },
   {
     relation: "신부 아버지",
     name: BRIDE_FATHER,
     phone: "010-0000-0000",
-    account: "KB은행 000819210205995000",
+    account: "KB 819210205995",
   },
   {
     relation: "신부 어머니",
     name: BRIDE_MOTHER,
     phone: "010-0000-0000",
-    account: "KB은행 00049780293117782000",
+    account: "KB 49780293117782",
   },
 ]
 
@@ -73,18 +73,18 @@ export const GROOM_INFO = [
     relation: "신랑",
     name: GROOM_FULLNAME,
     phone: "010-0000-0000",
-    account: "하나은행 00032291037803507000",
+    account: "하나은행 32291037803507",
   },
   {
     relation: "신랑 아버지",
     name: GROOM_FATHER,
     phone: "010-0000-0000",
-    account: "신한은행 00040104536863000",
+    account: "신한은행 40104536863",
   },
   {
     relation: "신랑 어머니",
     name: GROOM_MOTHER,
     phone: "010-0000-0000",
-    account: "국민은행 00080000201031159000",
+    account: "국민은행 80000201031159",
   },
 ]
