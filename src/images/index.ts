e04fb6248dd621b302816cdf805c763b55afbc87
SeongminYoonha <unisgene@gmail.com>
2026-04-11 11:27:04 +0900

    Replace PNG images with WEBP format

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,16 +1,16 @@
-import coverImage from "./cover.png"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import coverImage from "./cover.webp"
+import image1 from "./gallery-1.webp"
+import image2 from "./gallery-2.webp"
+import image3 from "./gallery-3.webp"
+import image4 from "./gallery-4.webp"
+import image5 from "./gallery-5.webp"
+import image6 from "./gallery-6.webp"
+import image7 from "./gallery-7.webp"
+import image8 from "./gallery-8.webp"
+import image9 from "./gallery-9.webp"
+/import image10 from "./image10.png"
+/import image11 from "./image11.png"
+/import image12 from "./image12.png"
 
 export const COVER_IMAGE = coverImage
 
@@ -24,7 +24,7 @@ export const GALLERY_IMAGES = [
   image7,
   image8,
   image9,
-  image10,
-  image11,
-  image12,
+/  image10,
+/  image11,
+/  image12,
 ]
