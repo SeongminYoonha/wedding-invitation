7df5168915d6d17803753a964b861d22763be2bf
SeongminYoonha <unisgene@gmail.com>
2026-04-11 11:32:57 +0900

    Disable image imports in index.ts
    Comment out image imports and references in index.ts

--- src/images/index.ts
+++ src/images/index.ts
@@ -8,9 +8,9 @@ import image6 from "./gallery-6.webp"
 import image7 from "./gallery-7.webp"
 import image8 from "./gallery-8.webp"
 import image9 from "./gallery-9.webp"
-/import image10 from "./image10.png"
-/import image11 from "./image11.png"
-/import image12 from "./image12.png"
+//import image10 from "./image10.png"
+//import image11 from "./image11.png"
+//import image12 from "./image12.png"
 
 export const COVER_IMAGE = coverImage
 
@@ -24,7 +24,7 @@ export const GALLERY_IMAGES = [
   image7,
   image8,
   image9,
-/  image10,
-/  image11,
-/  image12,
+//  image10,
+//  image11,
+//  image12,
 ]
