66f80feb8bb9c9142d6df5822deed4e5307944b0
SeongminYoonha <unisgene@gmail.com>
2026-04-11 13:23:12 +0900

    Update invitation text and comment out old content
    Commented out previous invitation text and updated some content.

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -25,15 +25,23 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      <div className="content">싱그러운 여름 향기 가득한 날</div>
-      <div className="content">소중한 분들을 모시고</div>
-      <div className="content">사랑의 약속을 하려고 합니다.</div>
+      //<div className="content">싱그러운 여름 향기 가득한 날</div>
+      //<div className="content">소중한 분들을 모시고</div>
+      //<div className="content">사랑의 약속을 하려고 합니다.</div>
+      //<div className="break" />
+      //<div className="content">햇살이 뜨거울 땐 가려주고,</div>
+      //<div className="content">비가 오면 우산이 되어주는</div>
+      //<div className="content">부부가 되겠습니다.</div>
+      //<div className="break" />
+      //<div className="content">기쁜날 함께 하시어</div>
+      //<div className="content">저희의 앞날을 축복해 주세요.</div>
+      <div className="content">도쿄에서 서로의 앞날을 고민하던 시절,</div>
+      <div className="content">옆에 머물러준 사람이 있었습니다.</div>
       <div className="break" />
-      <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-      <div className="content">비가 오면 우산이 되어주는</div>
-      <div className="content">부부가 되겠습니다.</div>
+      <div className="content">그 시간의 결을 따라,</div>
+      <div className="content">함께 살아가려 합니다.</div>
       <div className="break" />
-      <div className="content">기쁜날 함께 하시어</div>
+      <div className="content">기쁜날 함께 하셔서</div>
       <div className="content">저희의 앞날을 축복해 주세요.</div>
 
       <div className="break" />
